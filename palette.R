df0527fb21635dba25170c1810c828e6ec6e89d0
Austin Deans <austindeans@github.io>
2018-08-27 11:44:34 -0400

    palette work

--- palette.R
+++ palette.R
@@ -1,3 +1,9 @@
-rnorange <- #B1541B
-rndarkblue <- #0C2337
-rnlightblue <- #395675
+rnorange <- #AE541C
+rndarkblue <- #072F5C
+rnlightblue <- #556C84
+rntan <- #E4D9CD
+
+library(ggplot2)
+    
+ggplot(mtcars, aes(mpg, hp))+
+  geom_point(color = "rnorange")
